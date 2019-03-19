953210e6ad649e1ac7eed3514008c65daa8dd5c0
KRAJACOB <krakouassijacob@yahoo.fr>
2019-03-19 08:14:02 +0000

    changer le nom de la fonction get_age en calculAge, ajout de 'ans' a la fin de l'age

--- age.py
+++ age.py
@@ -1,12 +1,12 @@
 from datetime import date
 
-def get_age(annee_naissance):	
+def calculAage(annee_naissance):	
 	date_jour = date.today().year
 	age = date_jour - annee_naissance
 	return age
 
 def main():
 	annee_naissance = input("Enter votre annee de naissance : ")	
-	print("{} est {}".format("votre age ", get_age(int(annee_naissance))))
+	print("{} est {} {}".format("votre age ", calculAage(int(annee_naissance)), "ans"))
 
 main()			
